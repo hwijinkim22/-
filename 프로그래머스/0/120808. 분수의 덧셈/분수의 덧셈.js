6d1173da041ddebef6535a61a25a6218da6af003
hwijinkim22 <160462935+hwijinkim22@users.noreply.github.com>
2024-04-08 17:03:05 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,12 +1,12 @@
-
-function fnGCD(a, b){
-    return (a%b)? fnGCD(b, a%b) : b;
+function cal_gcd(a, b) {
+    return a % b === 0 ? b : cal_gcd(b, a % b)
 }
 
 function solution(denum1, num1, denum2, num2) {
-    let denum = denum1*num2 + denum2*num1;
+    let denum = denum1 * num2 + denum2 * num1;
     let num = num1 * num2;
-    let gcd = fnGCD(denum, num); //최대공약수
-
-    return [denum/gcd, num/gcd];
+    let gcd = cal_gcd(denum, num);
+    
+    // 최대 공약수를 분자 분모에 나누고 배열에 넣기
+    return [denum / gcd, num / gcd]
 }
